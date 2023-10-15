6043688ca9f03a26fa02a317c9af9f60c069ac89
Paranthaman117 <124195823+Paranthaman117@users.noreply.github.com>
2023-10-15 20:36:43 +0530

    guess.php

--- guess.php
+++ guess.php
@@ -1,37 +1,25 @@
 <html>
 <head>
-<title>  b4f6c389</title>
+<title>Guessing Game for Charles Severance</title>
 </head>
 <body>
 <h1>Welcome to my guessing game</h1>
 <p>
 <?php
-$correctnumber=56; 
-if(isset($_GET['guess']))
-{
-  if (is_numeric($_GET['guess'])===FALSE)
- {
-echo "Your guess is not a number";
-}
-else if ($_GET['guess']<56)
- {
-echo "Your guess is too low";
-}
-else if ($_GET['guess']>56)
- {
-echo "Your guess is too high";
-}
-else if ($_GET['guess']==56)
- {
-echo "Congratulations - You are right";
-}
-}
-else
-{
-echo "Missing guess parameter <br> Your guess is too short";
-}
+  if ( ! isset($_GET['guess']) ) { 
+    echo("Missing guess parameter");
+  } else if ( strlen($_GET['guess']) < 1 ) {
+    echo("Your guess is too short");
+  } else if ( ! is_numeric($_GET['guess']) ) {
+    echo("Your guess is not a number");
+  } else if ( $_GET['guess'] < 56 ) {
+    echo("Your guess is too low");
+  } else if ( $_GET['guess'] > 56 ) {
+    echo("Your guess is too high");
+  } else {
+    echo("Congratulations - You are right");
+  }
 ?>
 </p>
 </body>
 </html>
-  
